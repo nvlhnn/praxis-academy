f5363033c205538da6e2b664a86073fd6b9c88a8
nvlhnn <hanannaufal17@gmail.com>
2020-02-19 17:52:01 +0700

    first commit

---
+++ kemampuan-dasar-1/kasus/cari-java.bat
@@ -0,0 +1,24 @@
+﻿param($FolderPath)
+$Dir = get-childitem -path $FolderPath -recurse 
+$List = $Dir | where {$_.extension -eq ".java"} 
+if($list){
+$List | format-table Name 
+"ada file java pada direktori $FolderPath"
+ }
+ else{
+ "tidak ada file java"
+ }
+
+
+function TestPath()  
+{ 
+    $FileExists = Test-Path $FolderPath 
+    If ($FileExists -eq $True)  
+    { 
+        Return $true 
+    } 
+    Else  
+    { 
+        Return $false 
+    } 
+}
---
+++ kemampuan-dasar-1/kasus/file-ren.bat
@@ -0,0 +1,35 @@
+﻿param($FolderPath)
+$Dir = get-childitem -path $FolderPath -recurse 
+$List = $Dir | where {$_.extension -eq ".java"} 
+if($list)
+{
+foreach($i  in $list)
+{
+$ask = read-host "$i ada, ganti ? (y/t)"
+if($ask -eq "Y")
+{
+$newname = read-host "masukkan nama baru"
+Rename-Item -path "$FolderPath\$i" -newname "$newname.java"
+"Ganti Nama Berhasil, nama baru adalah $newname.java"
+}
+}
+}else{
+"tidak ada file java di folder $folderpath"}
+
+
+
+
+
+
+function TestPath()  
+{ 
+    $FileExists = Test-Path $FolderPath 
+    If ($FileExists -eq $True)  
+    { 
+        Return $true 
+    } 
+    Else  
+    { 
+        Return $false 
+    } 
+}
